02948c7a6b35d6ec1007e9be2fb6fd02f4df23b3
Juilliard <Juilliardwu0405@gmail.com>
2023-04-07 14:51:09 -0400

    Change main driver class for user to input their need

--- src/main/java/com/juilliardwu/module10assigment/Main.java
+++ src/main/java/com/juilliardwu/module10assigment/Main.java
@@ -1,5 +1,7 @@
 package com.juilliardwu.module10assigment;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,19 +14,25 @@ package com.juilliardwu.module10assigment;
  */
 public class Main {
     public static void main(String[] args) {
-        Professional p = new Professional();
-        p.pants();
-        p.shoes();
-        p.top();
-        Casual c = new Casual();
-        c.top();
-        c.shoes();
-        c.top();
-        Party partycloth = new Party();
-        partycloth.pants();
-        partycloth.shoes();
-        partycloth.top();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the type of cloth you will like");
+        String order = sc.nextLine();
+        switch(order){
+            case "Professional":
+                Professional p  = new Professional(); 
+                System.out.println("Professional clothes made");
+                break;
+            case "Party":
+                Party party = new Party();
+                System.out.println("Party clothes made");
+                break;
+            case "Casual":
+                Casual s = new Casual();
+                System.out.println("Casual clothes made");
+                break;
+            
+        }
+        
+            
     }
-    
-    
 }
